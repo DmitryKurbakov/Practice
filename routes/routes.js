c4e6751eaa0c0ef571f00f127166054b3add140b
budae <budaev.ant@gmail.com>
2017-08-13 23:04:33 +0300

    20170813-2303

--- routes/routes.js
+++ routes/routes.js
@@ -150,6 +150,85 @@ router.get('/apply', function (req, res) {
     return res.render('pages/proposal-create.ejs');
 });
 
+router.post('/submit-proposal', function (req, res) {
+
+    console.log(1);
+    fs = require("fs");
+    multiparty = require('multiparty');
+    var form = new multiparty.Form();
+    console.log(form);
+    //здесь будет храниться путь с загружаемому файлу, его тип и размер
+    var uploadFile = {uploadPath: '', type: '', size: 0};
+    //максимальный размер файла
+    var maxSize = 2 * 1024 * 1024; //2MB
+    //поддерживаемые типы(в данном случае это картинки формата jpeg,jpg и png)
+    var supportMimeTypes = ['application/msword'];
+    //массив с ошибками произошедшими в ходе загрузки файла
+    var errors = [];
+
+    //если произошла ошибка
+    form.on('error', function(err){
+        console.log('error0');
+        if(fs.existsSync(uploadFile.path)) {
+            //если загружаемый файл существует удаляем его
+            fs.unlinkSync(uploadFile.path);
+            console.log('error');
+        }
+    });
+
+    form.on('close', function() {
+        console.log('close');
+        //если нет ошибок и все хорошо
+        if(errors.length == 0) {
+            //сообщаем что все хорошо
+            res.send({status: 'ok', text: 'Success'});
+        }
+        else {
+            if(fs.existsSync(uploadFile.path)) {
+                //если загружаемый файл существует удаляем его
+                fs.unlinkSync(uploadFile.path);
+            }
+            //сообщаем что все плохо и какие произошли ошибки
+            res.send({status: 'bad', errors: errors});
+        }
+    });
+
+    // при поступление файла
+    form.on('part', function(part) {
+        console.log('part');
+        //читаем его размер в байтах
+        uploadFile.size = part.byteCount;
+        //читаем его тип
+        uploadFile.type = part.headers['content-type'];
+        //путь для сохранения файла
+        uploadFile.path = './files/' + part.filename;
+
+        //проверяем размер файла, он не должен быть больше максимального размера
+        if(uploadFile.size > maxSize) {
+            errors.push('File size is ' + uploadFile.size + '. Limit is' + (maxSize / 1024 / 1024) + 'MB.');
+        }
+
+        //проверяем является ли тип поддерживаемым
+        if(supportMimeTypes.indexOf(uploadFile.type) == -1) {
+            errors.push('Unsupported mimetype ' + uploadFile.type);
+        }
+
+        //если нет ошибок то создаем поток для записи файла
+        if(errors.length == 0) {
+            var out = fs.createWriteStream(uploadFile.path);
+            part.pipe(out);
+        }
+        else {
+            //пропускаем
+            //вообще здесь нужно как-то остановить загрузку и перейти к onclose
+            part.resume();
+        }
+    });
+    console.log('end');
+    // парсим форму
+    // console.log(parse(req));
+});
+
 router.get('/news', function (req, res) {
     dbutilities.getNews().then(function (r) {
         return res.render('pages/news.ejs', {
@@ -166,6 +245,11 @@ router.get('/news/:id', function (req, res) {
             item: r
         });
     });
+});
+
+router.get('/worksheet', function (req, res) {
+    var path = require('path');
+    return res.sendFile(path.resolve(__dirname + '/../public/sample.docx'));
 });
 //---------------------------API routes-----------------------------------------
 router.get('/api/users', function (req, res) {
