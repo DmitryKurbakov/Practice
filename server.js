75d413fea8b4c5aa347b1aa3c357f8743baf430f
Dmitry <dskurbakov@gmail.com>
2017-09-11 21:24:51 +0300

    Added SSL

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 /**
-*Module dependencies
-*/
+ *Module dependencies
+ */
 var
-  app = require('./app'),
-  http = require('http');
+    app = require('./app'),
+    https = require('https');
+    fs = require('fs');
+
+//==============================================================================
+var options = {
+    key: fs.readFileSync('./SSL/key.pem'),
+    cert: fs.readFileSync('./SSL/cert.pem'),
+    requestCert: false,
+    rejectUnauthorized: false
+};
+/**
+ *Create server instance
+ */
+var server = https.createServer(options, app);
+
+
 //==============================================================================
 /**
-*Create server instance
-*/
-var server = http.createServer(app);
-//==============================================================================
-/**
-*Module Variables
-*/
+ *Module Variables
+ */
 //==============================================================================
 var
-  port = app.get('port'),
-  env = app.get('env');
+    port = app.get('port'),
+    env = app.get('env');
 /**
-*Bind server to port
-*/
+ *Bind server to port
+ */
 //==============================================================================
 server.listen(port, function () {
-  return console.log('Xpress server listening on port:' + port +' in ' + env +
-  ' mode');
+    return console.log('Xpress server listening on port:' + port +' in ' + env +
+        ' mode');
 });
 //==============================================================================
